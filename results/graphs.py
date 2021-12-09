28a3ce880321dc5f73931b32f0e2f785ee83ec37
Artur <artur.cupelli@usp.br>
2021-12-09 16:43:24 -0300

    Run tests

---
+++ results/graphs.py
@@ -0,0 +1,10 @@
+from evalys.jobset import JobSet
+import matplotlib.pyplot as plt
+
+test = "02"
+
+js = JobSet.from_csv("./test{n}/test{n}_jobs.csv".format(n=test))
+
+js.plot(with_details=True)
+
+plt.savefig("./test{n}/results{n}.png".format(n=test))

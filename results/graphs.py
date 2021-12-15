35fc1f9c27d2b20a8cc51c9b00ff98751d9d167f
Artur <artur.cupelli@usp.br>
2021-12-15 13:37:06 -0300

    Run Test 03

--- results/graphs.py
+++ results/graphs.py
@@ -1,10 +1,10 @@
 from evalys.jobset import JobSet
 import matplotlib.pyplot as plt
 
-test = "02"
+test = "03"
 
 js = JobSet.from_csv("./test{n}/test{n}_jobs.csv".format(n=test))
 
-js.plot(with_details=True)
+js.plot(with_details=True, )
 
 plt.savefig("./test{n}/results{n}.png".format(n=test))
